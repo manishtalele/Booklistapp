cf105e230ffc0870bb39a42ddf9fb232b9ffa65c
tejastamkar <72966382+tejastamkar@users.noreply.github.com>
2021-09-17 14:22:30 +0530

    added the basic code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function BookList() {
+  return (
+    <section className="grid">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+  return (
+    <article className="book">
+      <BookImage></BookImage>
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const BookImage = () => {
+  return (
+    <img
+      src="https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg"
+      alt=""
+    />
+  );
+};
+
+const Title = () => <h1>Ikigai the My Japanese Book</h1>;
+
+const Author = () => <h2 className="author">Hector Garcia</h2>;
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
